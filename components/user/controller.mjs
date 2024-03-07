0d4a1b5d68f2823391ab9761fd0db1836a519246
Wayer52 <juanjsaavedra52@gmail.com>
2024-03-07 14:50:30 -0500

    Solucione los mensajes de error

--- components/user/controller.mjs
+++ components/user/controller.mjs
@@ -1,36 +1,27 @@
 import { isCorrectPassword, saveUser } from './store.mjs';
 
 // Lógica para el controlador de login
-const login = async (req, res) => {
-    const { user, password } = req.body;
+const login = async (body) => {
+    const { user, password } = body;
     if (!user || !password) {
-        return res.status(400).send('Faltan datos');
+        throw { status: 400, message: 'Faltan datos' };
     }
-    try {
-        const isAuthenticated = await isCorrectPassword(user, password);
-        if (isAuthenticated) {
-            return res.status(200).send('Usuario logeado correctamente');
-        } else {
-            return res.status(401).send('Usuario o contraseña incorrectos');
-        }
-    } catch (error) {
-        return res.status(500).send('Error interno');
+    const isAuthenticated = await isCorrectPassword(user, password);
+    if (isAuthenticated) {
+        return { status: 200, message: 'Usuario logeado correctamente' };
+    } else {
+        throw { status: 401, message: 'Usuario o contraseña incorrectos' };
     }
 };
 
 // Lógica para el controlador de register
-const register = async (req, res) => {
-    const { user, password } = req.body;
-    try {
-        const result = await saveUser(user, password);
-        if (result === 'Nombre de usuario ya registrado') {
-            res.status(400).send(result); // Envía un mensaje de error con código 400 (Bad Request)
-        } else {
-            res.status(201).send(result); // Envía una respuesta exitosa con código 201 (Created)
-        }
-    } catch (error) {
-        console.error('Error en register:', error);
-        res.status(500).send('Error interno'); // Envía un mensaje de error genérico con código 500 (Internal Server Error)
+const register = async (body) => {
+    const { user, password } = body;
+    const result = await saveUser(user, password);
+    if (result === 'Nombre de usuario ya registrado') {
+        throw { status: 400, message: result };
+    } else {
+        return { status: 201, message: result };
     }
 };
 
--- components/user/network.mjs
+++ components/user/network.mjs
@@ -10,19 +10,23 @@ router.get('/', (req, res) => {
 });
 
 router.post('/login', (req, res) => {
-    controller.login(req, res).then((data) => {
-        success(res, data, 200);
-    } ).catch((e) => {
-        error(res, 'Error interno', 500, e);
-    });
+    controller.login(req.body)
+        .then(({ status, message }) => {
+            success(res, message, status);
+        })
+        .catch(({ status, message }) => {
+            error(res, 'Error interno', status || 500, message);
+        });
 });
 
 router.post('/register', (req, res) => {
-    controller.register(req, res).then((data) => {
-        success(res, data, 201);
-    } ).catch((e) => {
-        error(res, 'Error interno', 500, e);
-    });
+    controller.register(req.body)
+        .then(({ status, message }) => {
+            success(res, message, status);
+        })
+        .catch(({ status, message }) => {
+            error(res, 'Error interno', status || 500, message);
+        });
 });
 
 export { router };
